c162fb5a7d4ef9728e572ea92d3bc70a3cd63d49
ujunglim <ujunglim@naver.com>
2021-01-25 21:50:07 +0800

    version 1.0.1

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,10 +1,23 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import './Movie.css';
+import React from "react";
+import PropTypes from "prop-types";
+import "./Movie.css";
+import { Link } from "react-router-dom";
 
-function Movie({title, year, summary, poster, genres}) {
-    return (
-			<div className="movie">
+function Movie({id, title, year, summary, poster, genres}) {
+	return (
+		<div className="movie">
+			<Link 
+				to={{
+					pathname: `/movie/${id}`,
+					state: {
+						title,   //title: title
+						year,
+						summary, 
+						poster,
+						genres
+					}
+				}}
+			>
 				<img src={poster} alt={title} title={title}/>
 				<div className="movie_data">
 					<h3 className="movie_title">{title}</h3>
@@ -16,8 +29,10 @@ function Movie({title, year, summary, poster, genres}) {
 					</ul>
 					<p className="movie_summary">{summary.slice(0, 180)}...</p>
 				</div>
-			</div>
-    );
+			</Link>
+		</div>
+		
+	);
 }
 
 Movie.propTypes = {
